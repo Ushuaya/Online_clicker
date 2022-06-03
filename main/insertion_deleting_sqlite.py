fcf3bd54325753eaaba0ffbb38be797397bff6af
Ivan Ushakov <vaniaushakov20@mail.ru>
2022-06-03 18:40:22 +0300

    Made delete command in telegram

--- main/insertion_deleting_sqlite.py
+++ main/insertion_deleting_sqlite.py
@@ -1,6 +1,6 @@
 import sqlite3
-from matplotlib.pyplot import text
-import pandas
+#from matplotlib.pyplot import text
+#import pandas
 #from sqlalchemy import create_engine
 import telebot
 from restricted_area import token_crypto
@@ -16,25 +16,26 @@ bot = telebot.TeleBot(API_TOKEN)
 
 @bot.message_handler(commands = ["start"])
 def start(messg): 
-
-    #cursor.execute("CREATE TABLE IF NOT EXISTS login_id(id INTEGER)") 
-    #connect.commit() 
+    connect = sqlite3.connect('users.db')
+    cursor = connect.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS login_id(id INTEGER)") 
+    connect.commit() 
     people_id = messg.chat.id 
-    # cursor.execute(f"SELECT id FROM login_id WHERE id = {people_id}")
-    # people_id_data = cursor.fetchone() 
+    cursor.execute(f"SELECT id FROM login_id WHERE id = {people_id}")
+    people_id_data = cursor.fetchone() 
     bot.send_message(messg.chat.id, "Type \"/find\" to start search of porch lock-code")
 
-    # if people_id_data is None: 
-    #     user_id = [messg.chat.id]
-    #     cursor.execute("INSERT INTO login_id VALUES(?);", user_id)
-    #     connect.commit() 
-    # else: 
-    #     bot.send_message(messg.chat.id, "Уже есть такой")
+    if people_id_data is None: 
+        user_id = [messg.chat.id]
+        cursor.execute("INSERT INTO login_id VALUES(?);", user_id)
+        connect.commit() 
+    else: 
+        bot.send_message(messg.chat.id, "Уже есть такой")
 
-@bot.message_handler(commands = ["find"])
-def start(messg): 
-    connect = sqlite3.connect('./DB/test.db')
-    cursor = connect.cursor()
+# @bot.message_handler(commands = ["find"])
+# def start(messg): 
+#     connect = sqlite3.connect('./DB/test.db')
+#     cursor = connect.cursor()
 
 
 
@@ -46,26 +47,29 @@ def listener(messages):
 
 
 bot.set_update_listener(listener)
-bot.polling() 
 
 
 
-# @bot.message_handler(commands = ["delete"])
-# def delete(messg): 
-#     connect = sqlite3.connect('users.db')
-#     cursor = connect.cursor()
 
-#     people_id = messg.chat.id 
-#     cursor.execute(f"SELECT id FROM login_id WHERE id = {people_id}")
-#     people_id_data = cursor.fetchone()
+@bot.message_handler(commands = ["delete"])
+def delete(messg): 
+    bot.send_message(messg.chat.id, f"Deleting")
+    connect = sqlite3.connect('users.db')
+    cursor = connect.cursor()
+
+    people_id = messg.chat.id 
+    cursor.execute(f"SELECT id FROM login_id WHERE id = {people_id}")
+    people_id_data = cursor.fetchone()
     
     
 
-#     if people_id_data is None: 
-#         bot.send_message(messg.chat.id, "NO SUCH USER IN DATABASE") 
-#     else: 
-#         cursor.execute(f"DELETE FROM login_id WHERE id = {people_id}")
-#         bot.send_message(messg.chat.id, f"Deleted user {people_id}")
-#         connect.commit() 
+    if people_id_data is None: 
+        bot.send_message(messg.chat.id, "NO SUCH USER IN DATABASE") 
+    else: 
+        cursor.execute(f"DELETE FROM login_id WHERE id = {people_id}")
+        bot.send_message(messg.chat.id, f"Deleted user {people_id}")
+        connect.commit() 
     
-#     connect.commit() 
+    connect.commit() 
+
+bot.polling()
