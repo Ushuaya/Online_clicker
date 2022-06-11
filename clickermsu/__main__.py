94ce908bdfa8fb04816bb984d21f0ac1eca30e1a
Ivan Ushakov <vaniaushakov20@mail.ru>
2022-06-11 20:18:10 +0300

    main docs

---
+++ clickermsu/__main__.py
@@ -0,0 +1,13 @@
+"""__main__..."""
+
+import pygame as pg
+from .__init__ import Game
+import sys
+
+if __name__ == "__main__":
+    pg.init()
+    pg.mixer.init()
+
+    Game()
+    pg.quit()
+    sys.exit(0)
